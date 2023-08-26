d920709b27e6e79fa95c84ddb78ee84fdcbcf758
Atakan Cinar <atakanccnar@outlook.com>
2023-08-26 21:35:28 +0300

    Adding Health Bar UI and adding safe area assets for different devices
    Safe area: On some displays, certain areas of the screen might not be visible to the user. This might be because the display's shape is non-rectangular or in the case of TV displays, it can be caused by overscan.

--- Assets/Scripts/Player/PlayerStats.cs
+++ Assets/Scripts/Player/PlayerStats.cs
@@ -1,19 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class PlayerStats : MonoBehaviour
 {
     [SerializeField] private float maxHealth;
     private float currentHealth;
 
+    [SerializeField] private Image healthFill;
+
     private void Start()
     {
         currentHealth = maxHealth;
+        healthFill.fillAmount = currentHealth / maxHealth;
     }
     public void PlayerTakeDamage(float _damage)
     {
         currentHealth -= _damage;
+        healthFill.fillAmount = currentHealth /maxHealth;
 
         if(currentHealth <= 0)
         {
