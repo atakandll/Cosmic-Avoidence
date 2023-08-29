f988fffacc309cd5a47a22ea83430152ae5a7de3
Atakan Cinar <atakanccnar@outlook.com>
2023-08-29 18:57:38 +0300

    UI Panel Controller- Text Mesh Pro

---
+++ Assets/Scripts/UI/PanelController.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanelController : MonoBehaviour
+{
+    [SerializeField] private CanvasGroup cGroup;
+    [SerializeField] private GameObject winScreen;
+    [SerializeField] private GameObject loseScreen;
+    void Start()
+    {
+        
+    }
+    public void ActiveWinScreen()
+    {
+        cGroup.alpha = 1.0f;
+        winScreen.SetActive(true);
+
+    }
+    public void ActiveLooseScreen()
+    {
+        cGroup.alpha = 1.0f;
+        loseScreen.SetActive(true);
+    }
+
+   
+}
