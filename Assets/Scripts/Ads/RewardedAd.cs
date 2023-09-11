b26216cdc972d9d552af671d52de1ff46936479a
atakandll <atakanccnar@outlook.com>
2023-09-11 17:29:53 +0300

    Rewarded Ads p1

---
+++ Assets/Scripts/Ads/RewardedAd.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Advertisements;
+
+public class RewardedAd : MonoBehaviour, IUnityAdsLoadListener,IUnityAdsShowListener
+{
+    [SerializeField] string _androidAdUnitId = "Rewarded_Android";
+    [SerializeField] string _iOSAdUnitId = "Rewarded_iOS";
+    string _adUnitId = null; // This will remain null for unsupported platforms
+ 
+    void Awake()
+    {   
+        // Get the Ad Unit ID for the current platform:
+#if UNITY_IOS
+        _adUnitId = _iOSAdUnitId;
+#elif UNITY_ANDROID
+        _adUnitId = _androidAdUnitId;
+#endif
+        
+        
+    }
+
+    public void LoadAd()
+    {
+        if(Advertisement.isInitialized)
+            Advertisement.Load(_adUnitId,this);
+        
+    }
+
+    public void ShowAd()
+    {
+        Advertisement.Show(_adUnitId,this);
+    }
+
+    public void OnUnityAdsAdLoaded(string placementId)
+    {
+    }
+
+    public void OnUnityAdsFailedToLoad(string placementId, UnityAdsLoadError error, string message)
+    {
+    }
+
+    public void OnUnityAdsShowFailure(string placementId, UnityAdsShowError error, string message)
+    {
+    }
+
+    public void OnUnityAdsShowStart(string placementId)
+    {
+    }
+
+    public void OnUnityAdsShowClick(string placementId)
+    {
+    }
+
+    public void OnUnityAdsShowComplete(string placementId, UnityAdsShowCompletionState showCompletionState)
+    {
+        //grant a reward
+    }
+}
